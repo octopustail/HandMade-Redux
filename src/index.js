ca77f363cd566d8d96baaa25ea9f52897e62cbc4
octopustail <494479914@qq.com>
2018-09-04 17:09:02 +0800

    组件通过context访问父亲组件state从而修改颜色

--- src/index.js
+++ src/index.js
@@ -1,84 +1,44 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import Header from "./Header";
+import Content from "./content";
+import './index.css'
 import createStore from './createStore'
 
+class Index extends Component {
+    static  childContextTypes = {
+        store: PropTypes.object
+    };
 
-function stateChanger(state, action) {
+    getChildContext(){
+        return { store}
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Content/>
+            </div>
+        )
+    }
+}
+
+const themeReducer = (state, action) => {
     if (!state) {
-        return {
-            title: {
-                text: 'React.js 小书',
-                color: 'red'
-            },
-            content: {
-                text: 'React.js 小书内容',
-                color: 'blue'
-            }
-        };
+        return {themeColor: 'red'}
     }
 
     switch (action.type) {
-        case 'UPDART_TITLE_TEXT':
-            return {
-                ...state,
-                title: {
-                    ...state.title,
-                    text: action.text
-                },
-            };
-        case 'UPDATE_TITLE_COLOR': {
-            return {
-                ...state,
-                title: {
-                    ...state.title,
-                    color: action.color
-                }
-            }
-        }
-
+        case 'CHANGE_COLOR':
+            return {...state, themeColor: action.themeColor}
         default:
             return state
-
     }
-}
 
-const store = createStore(stateChanger);
-//添加观察者，监听函数
-let oldState = store.getState()
-store.subscribe(() => {
-        const newState = store.getState();
-        renderApp(newState, oldState);
-        oldState = newState;
-    }
-);
+};
 
+const store = createStore(themeReducer);
 
-//实际执行渲染到DOM
-function renderTitle(newTitle, oldTitle = {}) {
-    if (newTitle === oldTitle) return;
-
-    console.log('render Title')
-    const titleDOM = document.getElementById('title');
-    titleDOM.innerHTML = newTitle.text;
-    titleDOM.style.color = oldTitle.color;
-}
-
-function renderContent(newContent, oldContent = {}) {
-    if (newContent === oldContent) return;
-    console.log('render Content');
-
-    const titleDOM = document.getElementById('content');
-    titleDOM.innerHTML = newContent.text;
-    titleDOM.style.color = newContent.color;
-}
-
-function renderApp(newAppState, oldAppState = {}) {
-    if (newAppState === oldAppState) return;
-    console.log('render App');
-
-    renderTitle(newAppState.title, oldAppState.title);
-    renderContent(newAppState.content, oldAppState.content);
-}
-
-
-renderApp(store.getState());
-store.dispatch({type: 'UPDART_TITLE_TEXT', text: '《React.js 小书》'});
-store.dispatch({type: 'UPDATE_TITLE_COLOR', color: 'blue'});
+ReactDOM.render(<Index/>, document.getElementById('root'));
